7911857f221e359a11053c5a52ada2107c4031f3
William Edwards <williamedwards7717@gmail.com>
2023-03-30 15:04:59 -0600

    rest of card

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="col-10 my-4 elevation-4">
-        <div class="d-flex">
-            <img class="img-fluid profile-img" :src="blog.creator.picture" :alt="blog.creator.name">
-            <h1>{{ blog.creator.name }}</h1>
+    <div class="col-10 my-4 elevation-4 d-flex justify-content-between p-4 rounded bg-white">
+        <div class="">
+            <div class="d-flex align-items-center">
+                <img class="img-fluid profile-img" :src="blog.creator.picture" :alt="blog.creator.name">
+                <h5 class="mx-3">{{ blog.creator.name }}</h5>
+            </div>
+            <div>
+                <p class="fw-bold">{{ blog.title }}</p>
+                <p>{{ blog.body.substring(0, 250) }} . . .</p>
+            </div>
+        </div>
+        <div>
+            <div>
+                <img class="blog-img" :src="blog.imgUrl" alt="">
+            </div>
         </div>
 
     </div>
@@ -28,4 +39,17 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.profile-img {
+    height: 5vh;
+    width: 5vh;
+    border-radius: 50%;
+}
+
+.blog-img {
+    height: 20vh;
+    widows: 20vh;
+    border-radius: 5px;
+
+}
+</style>
