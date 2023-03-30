329eddd635b3a03815fe2e77a17b3e1a1fab6682
LemonadeGT1 <michael.p.patience@gmail.com>
2023-03-30 16:15:44 -0600

    modal details updated

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,20 +3,32 @@
     <div class="modal-dialog modal-xl">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">{{ blog?.title }}</h1>
-
+          <h1 class="modal-title fs-3 mb-2" id="exampleModalLabel">{{ blog?.title }}</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">
-          <div class="d-flex justify-content-around">
-            <h6>Created By: {{ blog?.creator?.name }}</h6>
-            <h6>Created At:{{ blog?.createdAt }}</h6>
+        <div class="modal-body text-center">
+          <img :src="blog?.imgUrl" :alt="blog?.title" class="img-fluid mb-2 blog-img rounded">
+
+          <div class="d-flex justify-content-around row">
+            <div class="col-4">
+              <h6>Created By: <img :src="blog?.creator.picture" :alt="blog?.creator.name" class="profileImg"> {{
+                blog?.creator?.name }}</h6>
+            </div>
+            <div class="row">
+              <div class="col-12 text-start">
+                {{ blog.body }}
+              </div>
+            </div>
+            <div class="row justify-content-end">
+              <div class="col-2">Created At:{{ blog?.createdAt }}</div>
+            </div>
           </div>
-          <img class="blog-img my-3" v-for="p in blog?.blogImgs" :key="p" :src="p" alt="">
+          <!-- TODO - get all images/comments related to this blog -->
+          <!-- <img class="blog-img my-3" v-for="p in blog?.blogImgs" :key="p" :src="p" alt=""> -->
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
         </div>
       </div>
     </div>
@@ -37,9 +49,14 @@ export default {
 <style lang="scss" scoped>
 .blog-img {
   height: auto;
-  width: 100%;
+  width: 100vh;
   object-fit: cover;
   object-position: center;
 
+}
+
+.profileImg {
+  border-radius: 50%;
+  height: 5vh;
 }
 </style>
