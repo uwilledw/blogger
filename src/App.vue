63b881b8137532746ff49e8d58596161fa1fe89e
LemonadeGT1 <michael.p.patience@gmail.com>
2023-03-30 15:04:31 -0600

    modal

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@
   <main>
     <router-view />
   </main>
-   <footer class="bg-dark text-light">
+  <footer class="bg-dark text-light">
     Made with 💖 by CodeWorks
   </footer>
 </template>
+
+<Modal />
 
 <script>
 import { computed } from 'vue'
@@ -27,7 +29,7 @@ export default {
 <style lang="scss">
 @import "./assets/scss/main.scss";
 
-:root{
+:root {
   --main-height: calc(100vh - 32px - 64px);
 }
 
@@ -36,5 +38,9 @@ footer {
   display: grid;
   place-content: center;
   height: 32px;
+}
+
+main {
+  background-color: #fcd29f;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="modal fade" id="blogModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">{{ blog?.title }}</h1>
+
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="d-flex justify-content-around">
+            <h6>Created By: {{ blog?.creator?.name }}</h6>
+            <h6>Created At:{{ blog?.createdAt }}</h6>
+          </div>
+          <img class="blog-img my-3" v-for="p in blog?.blogImgs" :key="p" :src="p" alt="">
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { computed } from 'vue';
+import { AppState } from '../AppState.js';
+export default {
+  setup() {
+    return {
+      blog: computed(() => AppState.activeBlog)
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.blog-img {
+  height: auto;
+  width: 100%;
+  object-fit: cover;
+  object-position: center;
+
+}
+</style>
