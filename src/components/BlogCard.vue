70017d7e3d532954f332dac9941b8a3247dc4e5f
William Edwards <williamedwards7717@gmail.com>
2023-03-30 15:13:33 -0600

    styling

--- src/components/BlogCard.vue
+++ src/components/BlogCard.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="col-10 my-4 elevation-4 d-flex justify-content-between p-4 rounded bg-white">
-        <div class="">
-            <div class="d-flex align-items-center">
+    <div class="col-10 my-4 elevation-4 d-flex justify-content-between align-items-center p-4 rounded bg-white">
+        <div class="m-2">
+            <div class="d-flex align-items-center p-3 selectable rounded mb-3">
                 <img class="img-fluid profile-img" :src="blog.creator.picture" :alt="blog.creator.name">
                 <h5 class="mx-3">{{ blog.creator.name }}</h5>
             </div>
-            <div>
+            <div class="selectable p-1 rounded">
                 <p class="fw-bold">{{ blog.title }}</p>
                 <p>{{ blog.body.substring(0, 250) }} . . .</p>
             </div>
@@ -48,7 +48,7 @@ export default {
 
 .blog-img {
     height: 20vh;
-    widows: 20vh;
+    width: auto;
     border-radius: 5px;
 
 }
